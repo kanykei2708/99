2943f203ca7fc62648c7cff31a1c090e720138b7
Kani Omurzakova <kkadyrberdieva@gmail.com>
2019-05-10 00:10:58 +0000

    changes to method

--- 99.js
+++ 99.js
@@ -1,1 +1,9 @@
-Kani 
+
+//start with a variable
+var numberOfBottle = 99;
+var text = ""
+while (numberOfBottle > 0) {
+    text = "The number is " + numberOfBottle;
+    console.log(text)
+    numberOfBottle--;
+}
